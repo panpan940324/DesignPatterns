c836e0b5f42645d644e2f40c7be789e8801dd90f
wangpan <838347127@qq.com>
2022-10-04 18:22:03 +0800

    创建设计模式项目，第一次提交文件

---
+++ src/com/panpan/singleton/Main.java
@@ -0,0 +1,14 @@
+package com.panpan.singleton;
+
+/**
+ * @className: Main
+ * @Decsription: TODO
+ * @Author: 盼盼学Java
+ * @Date: 2022/10/4 18:20
+ * @version: 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
