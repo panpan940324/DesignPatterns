938c307823fcc7901e7580ad89b4a37072728af0
wangpan <838347127@qq.com>
2022-10-05 22:54:06 +0800

    补充策略模式

--- src/com/panpan/strategy/Main.java
+++ src/com/panpan/strategy/Main.java
@@ -8,6 +8,8 @@ import java.util.Arrays;
  *  其属于行为模式的一种，
  *  其用意是针对一组算法，将每一种算法封装到具有相同接口的独立类中，从而使得它们可以相互替代。
  *  策略模式使得算法可以在不影响客户端的情况下相互替代。
+ *
+ *  comparable接口不是一个策略模式，而comparator是一个策略模式
  * @Author: 盼盼学Java
  * @Date: 2022/10/5 16:12
  * @version: 1.0
@@ -23,6 +25,7 @@ public class Main {
         Dog[] arr = {new Dog(2,2), new Dog(1,3), new Dog(3,1)};
         System.out.println(Arrays.toString(arr));
         SorterV2<Dog> sorterV2 = new SorterV2<>();
+        // 这里其实可以使用一个Lambda表达式
         sorterV2.sort(arr, new CompareDogWeightStrategy());
         System.out.println(Arrays.toString(arr));
         sorterV2.sort(arr, new CompareDogHeightStrategy());
